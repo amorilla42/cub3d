58c794ccd1ebf4a82b4be4c03f7a90aefcdb9e5e
jdomingu98 <jadominguezarcas@gmail.com>
2023-07-20 15:39:32 +0200

    main function

--- src/cub3d.c
+++ src/cub3d.c
@@ -1,10 +1,36 @@
 #include "cub3d.h"
 
-int	main(int argc, char **argv)
+static void init_graphic_env(t_data *data)
 {
-	(void) argc;
-	(void) argv;
-	/*t_game	game;
+
+}
+
+int main(int argc, char **argv)
+{
+	t_data	*data;
+
+	if (argc != 2)
+		return (ft_putendl_fd(ARGS_ERR, STDERR_FILENO), EXIT_FAILURE);
+	data = ft_calloc(1, sizeof(t_data));
+	if (!data)
+		return (ft_putendl_fd(MALLOC_ERR, STDERR_FILENO), EXIT_FAILURE);
+	init_game_data(data);
+	data->mlx = mlx_init(WIDTH, HEIGHT, "cub3D", true);
+	if (!(data->mlx))
+	{
+		ft_putendl_fd(MALLOC_ERR, STDERR_FILENO);
+		free_and_exit(data, EXIT_FAILURE);
+	}
+	init_graphic_env(data);
+	mlx_key_hook(data->mlx, &key_func, data);
+	mlx_loop(data->mlx);
+	free_and_exit(data, EXIT_SUCCESS);
+}
+
+
+/*int	main(int argc, char **argv)
+{
+	t_game	game;
 
 	if (argc == 2)
 	{
@@ -21,7 +47,6 @@ int	main(int argc, char **argv)
 	}
 	else
 		ft_putstr_fd("Error\nWrong number of arguments\n", 2);
-
-	*/
 	return (0);
-}
+}*/
+
