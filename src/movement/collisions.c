5b2c882eb1406b7ce5968c7247d451efd9ff0e8a
Juan Antonio Domínguez <97060625+jdomingu98@users.noreply.github.com>
2023-08-26 01:53:44 +0200

    fix collisions

--- src/movement/collisions.c
+++ src/movement/collisions.c
@@ -2,10 +2,10 @@
 
 static int	is_valid_pos(t_data *data, double x, double y)
 {
-	return (x >= COLLIDER_DISTANCE && y >= COLLIDER_DISTANCE
-		&& x <= data->map_info->width - COLLIDER_DISTANCE
-		&& y <= data->map_info->height - COLLIDER_DISTANCE
-		&& (data->map[(int) x][(int) y] == 0
+	return (x > COLLIDER_DISTANCE && y > COLLIDER_DISTANCE
+		&& x < data->map_info->width - COLLIDER_DISTANCE * 2
+		&& y < data->map_info->height - COLLIDER_DISTANCE
+		&& (data->map[(int) x][(int) y] == '0'
 		|| ft_strchr("NSEW", data->map[(int) x][(int) y])));
 }
 
